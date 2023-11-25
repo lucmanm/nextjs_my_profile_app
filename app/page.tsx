80c24c9dbb8e9638c429030816784e57862de8c5
lucmanm <greenjamez22@gmail.com>
2023-11-26 02:14:37 +0300

    Updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import AboutSection from '@/components/about'
 import Hero from '@/components/hero'
-import Image from 'next/image'
+import Services from '@/components/services'
+import Work from '@/components/works'
 
 export default function Home() {
   return (
     <main>
       <Hero/>
       <AboutSection/>
+      <Services/>
+      <Work/>
     </main>
   )
 }
---
+++ components/project-card.tsx
@@ -0,0 +1,61 @@
+import Link from "next/link";
+import { Badge } from "./ui/badge";
+import { Card, CardHeader } from "./ui/card";
+import Image from "next/image";
+import { GithubIcon, Link2Icon } from "lucide-react";
+
+type ProjectDataProps = {
+  image: string;
+  category: string;
+  name: string;
+  description: string;
+  link: string;
+  github: string;
+};
+
+type ProjectProps = {
+  project: ProjectDataProps;
+};
+
+const ProjectCard: React.FC<ProjectProps> = ({ project }) => {
+  return (
+    <Card className="group overflow-hidden relative">
+      <CardHeader className="p-0">
+        <div className="relative w-full h-[300px] flex items-center justify-center bg-tertiary dark:bg-secondary/40 xl:bg-work_project_bg_light xl:dark:bg-work_project_bg_dark xl:bg-[110%] xl:bg-no-repeat overflow-hidden">
+          <Image
+            src={project.image}
+            width={247}
+            height={250}
+            alt={project.name}
+            priority
+            className="absolute bottom-0 shadow-2xl"
+          />
+
+          <div className="flex gap-x-4">
+            <Link
+              href={project.link}
+              className="bg-secondary w-[50px] h-[50px] rounded-full flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-200"
+            >
+              <Link2Icon className="text-black" />
+            </Link>
+            <Link
+              href={project.link}
+              className="bg-secondary w-[50px] h-[50px] rounded-full flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-200"
+            >
+              <GithubIcon className="text-black" />
+            </Link>
+          </div>
+        </div>
+      </CardHeader>
+      <div className="h-full px-8 py-6">
+        <Badge className="uppercase text-sm font-medium mb-2 absolute top-4 left-5">
+          {project.category}
+        </Badge>
+        <h4 className="h-4 mb-1">{project.name}</h4>
+        <h4 className="text-muted-foreground text-lg">{project.description}</h4>
+      </div>
+    </Card>
+  );
+};
+
+export default ProjectCard;
