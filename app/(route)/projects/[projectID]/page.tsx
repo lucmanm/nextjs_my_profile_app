dd22307248b40965180fca7da67af25945e17dbb
lucmanm <greenjamez22@gmail.com>
2024-06-29 02:04:49 +0300

    complete view projec

--- app/(route)/projects/[projectID]/page.tsx
+++ app/(route)/projects/[projectID]/page.tsx
@@ -1,6 +1,7 @@
 import { personalData } from "@/lib/const-data";
-import { Link } from "lucide-react";
+import { Github, Link as DirectLink } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 const Project = ({ params }: { params: { projectID: string } }) => {
@@ -17,30 +18,34 @@ const Project = ({ params }: { params: { projectID: string } }) => {
   }
 
   return (
-    <main className="container py-2 md:py-8 space-y-2">
+    <main className="container space-y-2 py-2 md:py-8">
       <Image
         src={project.image}
         alt={project.description}
         height={500}
         width={500}
-        className="h-[250px] w-full rounded-md object-none md:h-[500px] bg-slate-100/30 "
+        className="h-[250px] w-full rounded-md bg-slate-100/30 object-none md:h-[500px] "
       />
-      <div className="rounded-md bg-slate-100/30 p-4 py-2 md:p-8 md:py-8">
+      <div className="rounded-md bg-slate-100 p-4 py-2 md:p-8 md:py-8 space-y-2">
         <h1 className="h1">{project.name}</h1>
-        <h2 className="h2">{project.description}</h2>
-        <div className="flex items-center gap-2">
-          <Link className="h-3 w-4 md:h-4 md:w-4" />
-          <h3 className="h3">{project.link}</h3>
-        </div>
-        <div className="flex gap-2 py-2">
+        <div className="flex gap-2 py-2" >
           {project.teckStacks.map((item, id) => (
             <span
               key={id}
-              className="rounded-full bg-slate-100 px-2 py-0.5 text-xs md:text-base"
+              className="span rounded-full dark:bg-slate-100 bg-slate-950  "
             >
               {item}
             </span>
           ))}
+        </div>
+        <h2 className="h2">{project.description}</h2>
+        <div className="flex items-center gap-2">
+          <DirectLink className="icon" />
+          <Link href={project.link} className="p">{project.link}</Link>
+        </div>
+        <div className="flex items-center gap-2">
+          <Github className="icon" />
+          <Link href={project.github} className="p">{project.github}</Link>
         </div>
       </div>
     </main>
