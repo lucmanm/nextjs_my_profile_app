b70ea3d90abd14679adc3f599ce5cde530ca6fa2
lucmanm <greenjamez22@gmail.com>
2024-07-02 01:52:52 +0300

    fixed footer in fixed position

--- app/template.tsx
+++ app/template.tsx
@@ -20,13 +20,13 @@ const Template: React.FC<TemplateProps> = ({ children }) => {
         initial="hidden"
         animate="enter"
         transition={{ type: "linear", delay: 0.2, duration: 0.4 }}
-        className="dark:bg-background h-screen"
+        className="dark:bg-background min-h-screen"
       >
         {children}
       </motion.main>
-      <span
+      <motion.span
       style={{transform: `translateY(${completion - 100}%)`}}
-      className="fixed z-50 bg-primary w-1 top-0 right-0 bottom-0 transition-all duration-700 "></span>
+      className="fixed z-50 bg-primary w-1 top-0 right-0 bottom-0 transition-all duration-700 "></motion.span>
       <div className="min-h-full"></div>
     </>
   );
